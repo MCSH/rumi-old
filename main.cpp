ddf142c8c175253df281adff200e847e1b11cc16
Sajjad Heydari <mcshemail@gmail.com>
2019-04-06 16:18:33 +0430

    Added object generation

--- main.cpp
+++ main.cpp
@@ -1,4 +1,9 @@
 #include <fstream>
+#include "llvm/Support/FileSystem.h"
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Target/TargetMachine.h"
+#include "llvm/Support/TargetSelect.h"
+#include "llvm/Support/TargetRegistry.h"
 
 int yyparse();
 extern "C" FILE *yyin;
@@ -19,6 +24,57 @@ int main(int argc, char **argv){
     programBlock->codegen(cc);
 
     cc->module->print(llvm::outs(), nullptr);
+
+
+    // TODO place ment?
+
+    llvm::InitializeAllTargetInfos();
+    llvm::InitializeAllTargets();
+    llvm::InitializeAllTargetMCs();
+    llvm::InitializeAllAsmParsers();
+    llvm::InitializeAllAsmPrinters();
+
+    auto targetTriple = llvm::sys::getDefaultTargetTriple();
+
+    std::string Error;
+    auto target = llvm::TargetRegistry::lookupTarget(targetTriple, Error);
+
+    if(!target){
+        llvm::errs() << Error;
+        return 1;
+    }
+
+    auto CPU = "generic";
+    auto Features = "";
+
+    llvm::TargetOptions opt;
+    auto RM = llvm::Optional<llvm::Reloc::Model>();
+    auto targetMachine = target -> createTargetMachine(targetTriple, CPU, Features, opt, RM);
+
+    cc-> module->setDataLayout(targetMachine->createDataLayout());
+    cc-> module->setTargetTriple(targetTriple);
+
+
+
+    auto output = "out.o";
+    std::error_code EC;
+    llvm::raw_fd_ostream dest(output, EC, llvm::sys::fs::F_None);
+
+    if(EC){
+        llvm::errs() << "Could not open file: " << EC.message();
+        return 1;
+    }
+
+    llvm::legacy::PassManager pass;
+    auto fileType = llvm::TargetMachine::CGFT_ObjectFile;
+
+    if(targetMachine->addPassesToEmitFile(pass, dest, nullptr, fileType)){
+        llvm::errs() << "Target Machine can't emit a file of this type";
+        return 1;
+    }
+
+    pass.run(*(cc->module));
+    dest.flush();
 
     delete cc;
     return 0;
